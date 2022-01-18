dc7c2f208c47cdede537906e7f046f0d6e928f57
igor-ostojic <igorostojic1996@gmail.com>
2022-01-18 12:22:28 +0100

    3 files modified - Final Commit

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -6,27 +6,49 @@ const Shop = () => {
   const [shopData, setShopData] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  // Abort Controller for canceling the Fetch Request when the component using it unmounts
+  const abortController = new AbortController();
+
   useEffect(() => {
-    fetchData();
+    fetchData().catch((error) => console.log(error.message));
+    return () => abortController.abort();
   }, []);
 
   const fetchData = async () => {
     setLoading(true);
-    let response = await fetch('https://fakestoreapi.com/products');
+    let response = await fetch('https://fakestoreapi.com/products', {
+      signal: abortController.signal,
+    });
     let data = await response.json();
 
     setShopData(data);
     setLoading(false);
+
   };
 
   return (
-    <div className='content shop'>
+    <div className="content shop">
       <h1>Shop</h1>
-      {loading && <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>}
+      {loading && (
+        <div className="lds-spinner">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
       <div className="shop-data">
         {shopData &&
           shopData.map((item) => (
-            <div key={item.id} className='item-link'>
+            <div key={item.id} className="item-link">
               <Link to={`/shop/${item.id}`}>{item.title}</Link>
             </div>
           ))}
